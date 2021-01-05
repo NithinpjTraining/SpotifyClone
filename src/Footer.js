5fed75e4043523cc8086cae164d491a849e7e7aa
yamuna1996d <46672811+yamuna1996d@users.noreply.github.com>
2021-01-05 12:12:27 +0530

    first commit

--- src/Footer.js
+++ src/Footer.js
@@ -13,6 +13,7 @@ import { Grid, Slider } from "@material-ui/core";
 import { useDataLayerValue } from "./DataLayer";
 function Footer({ spotify }) {
     const [{ token, item, playing }, dispatch] = useDataLayerValue();
+
   const handlePlayPause = () => {
     if (playing) {
       spotify.pause();
@@ -59,45 +60,45 @@ function Footer({ spotify }) {
 
   return (
     <div className="footer">
-      <div className="footer__left">
+      <div className="footer_left">
         <img
-          className="footer__albumLogo"
+          className="footer_albumLogo"
           src={item?.album.images[0].url}
           alt={item?.name}
         />
         {item ? (
-          <div className="footer__songInfo">
+          <div className="footer_songInfo">
             <h4>{item.name}</h4>
             <p>{item.artists.map((artist) => artist.name).join(", ")}</p>
           </div>
         ) : (
-          <div className="footer__songInfo">
+          <div className="footer_songInfo">
             <h4>No song is playing</h4>
             <p>...</p>
           </div>
         )}
       </div>
 
-      <div className="footer__center">
-        <ShuffleIcon className="footer__green" />
-        <SkipPreviousIcon onClick={skipNext} className="footer__icon" />
+      <div className="footer_center">
+        <ShuffleIcon className="footer_green" />
+        <SkipPreviousIcon onClick={skipNext} className="footer_icon" />
         {playing ? (
           <PauseCircleOutlineIcon
             onClick={handlePlayPause}
             fontSize="large"
-            className="footer__icon"
+            className="footer_icon"
           />
         ) : (
           <PlayCircleOutlineIcon
             onClick={handlePlayPause}
             fontSize="large"
-            className="footer__icon"
+            className="footer_icon"
           />
         )}
-        <SkipNextIcon onClick={skipPrevious} className="footer__icon" />
-        <RepeatIcon className="footer__green" />
+        <SkipNextIcon onClick={skipPrevious} className="footer_icon" />
+        <RepeatIcon className="footer_green" />
       </div>
-      <div className="footer__right">
+      <div className="footer_right">
         <Grid container spacing={2}>
           <Grid item>
             <PlaylistPlayIcon />
